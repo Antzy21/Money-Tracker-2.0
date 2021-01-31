90cee2222fa041c6a3618c107ce032545fa4924d
Anthony Dunford <trashzy21@gmail.com>
2021-01-31 12:46:03 +0000

    Add Cors and configure launchSettings.json

--- Startup.cs
+++ Startup.cs
@@ -20,12 +20,26 @@ namespace Money_Tracker_2._0
             Configuration = configuration;
         }
 
+        public string clientUi = "client_ui";
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: clientUi,
+                    builder =>
+                    {
+                        builder.WithOrigins("http://localhost:8080",
+                                            "http://192.168.0.16:8080/")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .AllowCredentials();
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -34,6 +48,7 @@ namespace Money_Tracker_2._0
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseCors(clientUi);
             }
 
             app.UseHttpsRedirection();
@@ -46,6 +61,7 @@ namespace Money_Tracker_2._0
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }
