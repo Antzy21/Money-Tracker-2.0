44279c5331edab02ae4a27a9f41dafc8981e7170
Anthony Dunford <trashzy21@gmail.com>
2021-02-07 22:16:51 +0000

    Get basic Csv handler operational

--- Services/CsvService.cs
+++ Services/CsvService.cs
@@ -1,7 +1,5 @@
-﻿using CsvHelper;
-using MoneyTracker.Models;
+﻿using Microsoft.AspNetCore.Http;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 
@@ -13,13 +11,54 @@ namespace MoneyTracker.Services
         {
         }
 
-        public List<CsvTransaction> ReadCsv(string path)
+        public List<T> ReadCsvTo<T>(IFormFile file) where T : new()
         {
-            var reader = new StreamReader(path);
-            var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
-            var records = csv.GetRecords<CsvTransaction>().ToList();
+            Stream stream = file.OpenReadStream();
+            StreamReader streamReader = new StreamReader(stream);
 
-            return records;
+            var listOfObjects = new List<T>();
+
+            string line = streamReader.ReadLine();
+
+            var headers = line.Split(',');
+            var rows = new List<Dictionary<string, string>>();
+
+            line = streamReader.ReadLine();
+            while (line != null)
+            {
+                if (line.Trim().Length > 0)
+                {
+                    var row = new Dictionary<string, string>();
+                    foreach (var cell in line.Split(',').Select((l, i) => new { element = l, idx = i }))
+                    {
+                        row.Add(headers[cell.idx], cell.element);
+                    }
+                    rows.Add(row);
+                }
+                line = streamReader.ReadLine();
+            }
+
+            foreach (var row in rows)
+            {
+                listOfObjects.Add(ConvertLineTo<T>(row));
+            }
+
+            return listOfObjects;
+        }
+
+        private static T ConvertLineTo<T>(Dictionary<string, string> row) where T : new()
+        {
+            T obj = new T();
+
+            var properties = typeof(T).GetProperties();
+
+            foreach (var property in properties)
+            {
+                var entry = row[property.Name];
+                property.SetValue(obj, entry);
+            }
+
+            return obj;
         }
     }
 }
