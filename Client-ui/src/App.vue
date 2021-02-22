92abb435e1003105f687b8fb4a5741dd2c2d4a2b
Anthony Dunford <trashzy21@gmail.com>
2021-02-22 21:13:57 +0000

    Initial Styling

--- Client-ui/src/App.vue
+++ Client-ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/temp">Temp</router-link>
     </div>
     <router-view/>
@@ -24,23 +24,38 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background: #cee8f2;
+  height: 100%;
+}
+
+body {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: auto;
+  margin: 0px;
 }
 
 #nav {
-  padding: 30px;
+  background: #1F2E55;
+  padding: 20px;
 }
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #c1c9dc;
+  padding: 0 1em;
+  text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  opacity: 0.6;
 }
 </style>
