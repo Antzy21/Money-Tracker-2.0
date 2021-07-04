ef5d99d3f1e9fe34b516084e6a3bf74f66a64927
Anthony Dunford <trashzy21@gmail.com>
2021-07-04 21:17:30 +0100

    Add contactGroup page and use bootstrap containers

--- Client-ui/src/components/Contacts.vue
+++ Client-ui/src/components/Contacts.vue
@@ -1,48 +1,57 @@
 <template>
   <div class="container">
-    <h1>Contacts</h1>
-    <table>
-      <thead>
-        <tr>
-          <th>Id</th>
-          <th>Name</th>
-          <th>Group</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="contact in contacts" :key="contact.id">
-          <td>
-            {{contact.id}}
-          </td>
-          <td>
-            {{contact.name}}
-          </td>
-          <td>
-            {{contact.contactGroup}}
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button @click="onAddContact">
-              +
-            </button>
-          </td>
-          <td>
-            <input v-model="newContact.name">
-          </td>
-          <td>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="row">
+      <div class="col">
+        <h1>Contacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Group</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="contact in contacts" :key="contact.id">
+              <td>
+                {{contact.id}}
+              </td>
+              <td>
+                {{contact.name}}
+              </td>
+              <td>
+                {{contact.contactGroup}}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button @click="onAddContact">
+                  +
+                </button>
+              </td>
+              <td>
+                <input v-model="newContact.name">
+              </td>
+              <td>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <ContactGroupsTable />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
+import ContactGroupsTable from './contactReferences/ContactGroupsTable.vue'
 
 export default {
   name: 'Contacts',
+  components: {
+    ContactGroupsTable
+  },
   computed: {
     ...mapState(['contacts', 'contactGroups']),
   },
---
+++ Client-ui/src/components/contactReferences/ContactGroupsTable.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="col">
+        <h1>Groups</h1>
+        <table>
+            <thead>
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Colour</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="contactGroup in contactGroups" :key="contactGroup.id">
+                <td>
+                {{contactGroup.id}}
+                </td>
+                <td>
+                {{contactGroup.name}}
+                </td>
+                <td>
+                {{contactGroup.contactGroup}}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                <button @click="onAddContactGroup">
+                    +
+                </button>
+                </td>
+                <td>
+                <input v-model="newContactGroup.name">
+                </td>
+                <td>
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['contactGroups']),
+  },
+  data() {
+    return {
+      newContactGroup: {
+        name: '',
+        colour: '#ffffff',
+      }
+    }
+  },
+  methods: {
+    ...mapActions(['addContactGroup']),
+    onAddContactGroup() {
+      this.addContactGroup(this.newContactGroup);
+      this.resetNewContactGroup();
+    },
+    resetNewContactGroup() {
+      this.newContactGroup = {
+        name: '',
+        colour: '#ffffff',
+      }
+    },
+  }
+}
+</script>
