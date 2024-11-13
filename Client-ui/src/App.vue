5fd15b8bc463e4566163ead1695579301e174c2e
Anthony Dunford <trashzy21@gmail.com>
2024-11-13 20:06:46 +0000

    Migrate fully to TypeScript

--- Client-ui/src/App.vue
+++ Client-ui/src/App.vue
@@ -1,64 +1,44 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/temp">Temp</router-link>
-      <router-link to="/categories">Categories</router-link>
-      <router-link to="/todo">To Do</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-
-<script>
-import {mapActions} from 'vuex'
-import 'bootstrap/scss/bootstrap.scss'
-
-export default {
-  name: 'App',
-  created() {
-    this.initialise();
-  },
-  methods: {
-    ...mapActions(['initialise'])
-  },
-}
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  font-family: Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/categories">Categories</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  max-height: 100vh;
+}
+
+nav {
+  width: 100%;
   text-align: center;
-  color: #2c3e50;
-  background: #cee8f2;
-  height: 100%;
 }
 
-body {
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  overflow: auto;
-  margin: 0px;
+nav a.router-link-exact-active {
+  color: var(--color-text);
 }
 
-#nav {
-  background: #1F2E55;
-  padding: 20px;
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #c1c9dc;
-  padding: 0 1em;
-  text-decoration: none;
+nav a {
+  display: inline-block;
+  padding: 1rem;
+  color: var(--color-text);
 }
 
-#nav a.router-link-exact-active {
-  opacity: 0.6;
+nav a:first-of-type {
+  border: 0;
 }
 </style>
