1d2d6d435c143cfad61020d8dbec772bec515d89
Anthony Dunford <trashzy21@gmail.com>
2024-11-17 14:10:38 +0000

    Add delete category functionality

--- Client-ui/src/views/Categories.vue
+++ Client-ui/src/views/Categories.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { Category } from "@/types/category";
-import { getCategories, postCategory } from "@/api/CategoriesApi"
+import { deleteCategory, getCategories, postCategory } from "@/api/CategoriesApi"
 import { ref, type Ref } from "vue";
 
 var categories: Ref<Category[]> = ref([])
@@ -15,10 +15,18 @@ function handleEnter(event: Event) {
     const inputElement = (event.target as HTMLInputElement)
     inputElement.blur();
 
-    postCategory({name: newCategoryName})
+    postCategory({ name: newCategoryName })
         .then((newCategory) => {
             newCategoryName = ""
             categories.value.push(newCategory)
+        });
+}
+
+function handleDelete(category: Category) {
+    deleteCategory(category.id)
+        .then(() => {
+            const index = categories.value.findIndex(c => c.id === category.id)
+            categories.value.splice(index, 1)
         });
 }
 
@@ -31,10 +39,16 @@ function handleEnter(event: Event) {
         <tbody>
             <tr v-for="category in categories">
                 <td>{{ category.name }}</td>
+                <td>
+                    <button v-on:click="handleDelete(category)">
+                        Delete
+                    </button>
+                </td>
             </tr>
             <tr>
                 <td>
-                    <input placeholder="...new category" v-model="newCategoryName" v-on:keyup.enter="handleEnter($event)">
+                    <input placeholder="...new category" v-model="newCategoryName"
+                        v-on:keyup.enter="handleEnter($event)">
                 </td>
             </tr>
         </tbody>
