cf2100b6bb010a3516a4d00e1a96105ff4eebbfb
Anthony Dunford <trashzy21@gmail.com>
2024-11-29 21:30:20 +0000

    Add transaction list

--- Client-ui/src/App.vue
+++ Client-ui/src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
@@ -8,6 +7,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <nav>
       <RouterLink to="/">Home</RouterLink>
       <RouterLink to="/categories">Categories</RouterLink>
+      <RouterLink to="/transactions">Transactions</RouterLink>
     </nav>
   </header>
 
---
+++ Client-ui/src/components/TransactionItem.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import type { Transaction } from "@/types/transaction";
+import { computed } from "vue";
+
+const { transaction } = defineProps<{
+    transaction: Transaction,
+}>()
+
+</script>
+
+<template>
+    <td>
+        {{ transaction.contact }}
+    </td>
+    <td v-if="transaction.amount > 0" style="color: green;">
+        £{{ transaction.amount }}
+    </td>
+    <td v-if="transaction.amount < 0" style="color: red;">
+        -£{{ -transaction.amount }}
+    </td>
+</template>
