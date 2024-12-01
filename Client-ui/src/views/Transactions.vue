b9987493f3dc6e39708674afe7d7e6fb84371ba2
Anthony Dunford <trashzy21@gmail.com>
2024-12-01 14:28:40 +0000

    Add basic file uploader

--- Client-ui/src/views/Transactions.vue
+++ Client-ui/src/views/Transactions.vue
@@ -1,19 +1,36 @@
 <script setup lang="ts">
 import type { Transaction } from "@/types/transaction";
-import { getTransactions } from "@/api/TransactionsApi"
+import { getTransactions, postCsv } from "@/api/TransactionsApi"
 import { ref, type Ref } from "vue";
 import TransactionsItem from "@/components/TransactionItem.vue";
 
 var transactions: Ref<Transaction[]> = ref([])
 
-getTransactions().then((data: any[]) => {
-    transactions.value = data
-})
+function loadTransactions() {
+    getTransactions().then((data: any[]) => {
+        transactions.value = data
+    })
+}
+
+function onFileChanged($event: Event) {
+    const target = $event.target as HTMLInputElement;
+    if (target && target.files) {
+        postCsv(target.files[0]).then(() =>
+            loadTransactions()
+        )
+    }
+}
+    
+loadTransactions()
 
 </script>
 
 <template>
     <h1>Transactions</h1>
+    <div>
+        Upload Transactions
+        <input type="file" @change="onFileChanged($event)" accept=".csv*" capture />
+    </div>
     <div>There are {{ transactions.length }} transactions</div>
     <table>
         <tbody>
