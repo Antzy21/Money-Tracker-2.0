88a2ac680baeaebb3db65c0e958817d88df6a840
Anthony Dunford <trashzy21@gmail.com>
2025-03-14 21:26:18 +0000

    Backend controller functions to delete all

---
+++ Server/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MoneyTracker2.Data;
+
+namespace MoneyTracker2.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class AdminController(MoneyTrackerContext context) : ControllerBase
+{
+    [HttpDelete("DeleteAllTransactions")]
+    public async Task<ActionResult> DeleteTransactions()
+    {
+        context.Transactions.RemoveRange(context.Transactions);
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+
+    [HttpDelete("DeleteAllCategories")]
+    public async Task<ActionResult> DeleteAllCategories()
+    {
+        context.Categories.RemoveRange(context.Categories);
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+}
