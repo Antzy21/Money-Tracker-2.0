aca1148cbb143adea547f0cf1bea094ab6450fe0
= <=>
2025-08-25 14:40:46 +0000

    Use bootstrap nav styling

--- Client-ui/src/App.vue
+++ Client-ui/src/App.vue
@@ -5,40 +5,27 @@ import HealthCheck from './components/HealthCheck.vue'
 
 <template>
   <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/categories">Categories</RouterLink>
-      <RouterLink to="/transactions">Transactions</RouterLink>
-      <RouterLink to="/admin">Admin</RouterLink>
-      <HealthCheck></HealthCheck>
+    <nav class="navbar navbar-expand-sm">
+
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <RouterLink class="nav-link" to="/">Home</RouterLink>
+            <RouterLink class="nav-link" to="/categories">Categories</RouterLink>
+            <RouterLink class="nav-link" to="/transactions">Transactions</RouterLink>
+            <RouterLink class="nav-link" to="/admin">Admin</RouterLink>
+          </div>
+        </div>
+        <HealthCheck></HealthCheck>
+      </div>
     </nav>
+
+
+    
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  max-height: 100vh;
-}
-
-nav {
-  width: 100%;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-  font-weight: bold;
-  border-bottom: 2px solid var(--color-text);
-  background-color: var(--color-background-mute);
-}
-
-nav a {
-  text-decoration-line: none;
-  display: inline-block;
-  padding: 1rem;
-  color: var(--color-text);
-}
-
-</style>
